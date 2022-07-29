2b3b8cab3eb71395c3444b21e9e1d22c4df50d15
Ntambara Rukaka Steven <stevio052002@gmail.com>
2022-07-29 16:55:38 +0200

    Add files via upload

---
+++ sum.py
@@ -0,0 +1,11 @@
+#Write a program to remove characters from a string starting from zero up to n and return a new string.
+# string sample "i am going to kigali in the next two weeks"
+#zero means Position one that is i
+#up to n Position may be any position let's say 16 it is a(16)
+# a_string = "abcdefghijklmnopqrstuvwxyz"
+# sliced =a_string[15:]
+# print(sliced)
+
+word =str(input("Enter the full word: "))
+x = word[15:]
+print(x)
